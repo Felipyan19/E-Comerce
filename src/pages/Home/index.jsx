4d167aeedc1494bc5fec9f2045638f9c6eeb7b4d
FelipyanCCG <felipyan199@gmail.com>
2023-10-21 20:24:56 -0500

    update src/pages/Home cards and API patzi

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from 'react'
 import { Layout } from '../../Components/Layout'
+import { Card } from '../../Components/Card'
 function Home() {
+    const [items, setItems]=useState(null)
 
+    useEffect(()=>{
+       fetch('https://api.escuelajs.co/api/v1/products')
+        .then(res=>res.json())
+        .then(data=>setItems(data))
+    }, [])
     return (
      
         <Layout className='bg-red-100'>
-         soy Home
+        my home
+        {
+            items && items.map(item=>(
+                <Card key={item.id} item={item} />
+            ))
+        }
         </Layout>
     )
   }
