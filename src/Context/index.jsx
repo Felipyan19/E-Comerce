13d5dc18c9ea46362a6d1a47211ac507d88c80f5
FelipyanCCG <felipyan199@gmail.com>
2023-10-22 22:23:06 -0500

    update src/Context/index.jsx, src/pages/Home/index.jsx and src/pages/MyOrders/index.jsx filtered

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect  } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -25,24 +25,52 @@ export const ShoppingCartProvider = ({ children }) => {
     // Shopping Cart . order
     const [order, setOrder] = useState([])
 
-    return (
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow,
-            cartProducts,
-            setCartProducts,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            isCheckoutSideMenuOpen,
-            order,
-            setOrder
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
+    // Get products from API
+    const [items, setItems] = useState(null)
+
+    // Search by title filter
+    const [filteredItems, setFilteredItems] = useState(null)
+
+  // Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  useEffect(() => {
+    if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+  }, [items, searchByTitle])
+
+  return (
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts,
+      isCheckoutSideMenuOpen,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu,
+      order,
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle,
+      filteredItems
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -13,7 +13,7 @@ function MyOrders() {
 
         <Layout >
             <div className="flex items-center justify-center relative w-80 mb-4">
-                <h1>MyOrders</h1>
+                <h1 className="font-medium text-xl">MyOrders</h1>
             </div>
             {
                 context.order.map((product, index) => (
